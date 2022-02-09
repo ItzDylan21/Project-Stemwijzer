70851e982404139d45794b048fa2c78b1f7e050a
Dylan Worseling <dylan-dylan99@hotmail.com>
2022-02-09 01:05:17 +0100

    feat: configuration file for connecting to the database

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// Define credentials
+define('DB_SERVER', 'localhost');	
+define('DB_USERNAME', 'root');	
+define('DB_PASSWORD', '');	
+define('DB_NAME', '');	
+// Attempt connection
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if($conn == false)
+{
+	die("COULD NOT CONNECT. " .mysqli_connect_error());
+}
+?>
