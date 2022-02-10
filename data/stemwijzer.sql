3b403cee1f66126589dbe11fd5c9cc58aa0073d6
Dylan Worseling <dylan-dylan99@hotmail.com>
2022-02-10 13:41:41 +0100

    choir: updated sql file

--- data/stemwijzer.sql
+++ data/stemwijzer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 10, 2022 at 01:38 PM
+-- Generation Time: Feb 10, 2022 at 01:40 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.3.27
 
