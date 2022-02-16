625bd10722ca723a984c77f17bcf32b092ed5f93
Bas <b.vandervegt2014@gmail.com>
2022-02-16 13:04:12 +0100

    chore: updated database with foreign keys

--- data/vote.sql
+++ data/vote.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 11 feb 2022 om 16:00
--- Serverversie: 10.4.22-MariaDB
--- PHP-versie: 8.1.2
+-- Generation Time: Feb 16, 2022 at 12:58 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `municipality`
+-- Table structure for table `code`
+--
+
+CREATE TABLE `code` (
+  `codeID` int(10) NOT NULL,
+  `timeCreated` datetime NOT NULL,
+  `uniqueCode` varchar(32) NOT NULL,
+  `codeUsed` tinyint(1) NOT NULL,
+  `timeUsed` datetime NOT NULL,
+  `usedAfterExpired` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `memberID` int(4) NOT NULL,
+  `memberFirstName` varchar(128) NOT NULL,
+  `memberLastName` varchar(128) NOT NULL,
+  `memberPicture` varchar(128) NOT NULL,
+  `partyID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `municipality`
 --
 
 CREATE TABLE `municipality` (
@@ -35,47 +64,111 @@ CREATE TABLE `municipality` (
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `party`
+-- Table structure for table `party`
 --
 
 CREATE TABLE `party` (
   `partyID` int(11) NOT NULL,
   `partyname` varchar(64) NOT NULL,
   `partyinfo` varchar(1024) NOT NULL,
-  `partylogo` varchar(64) NOT NULL
+  `partylogo` varchar(64) NOT NULL,
+  `municipalityID` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote`
+--
+
+CREATE TABLE `vote` (
+  `voteID` int(10) NOT NULL,
+  `codeID` int(10) NOT NULL,
+  `memberID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `municipality`
+-- Indexes for table `code`
+--
+ALTER TABLE `code`
+  ADD PRIMARY KEY (`codeID`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`memberID`),
+  ADD KEY `party` (`partyID`);
+
+--
+-- Indexes for table `municipality`
 --
 ALTER TABLE `municipality`
   ADD PRIMARY KEY (`municipalityID`);
 
 --
--- Indexen voor tabel `party`
+-- Indexes for table `party`
 --
 ALTER TABLE `party`
-  ADD PRIMARY KEY (`partyID`);
+  ADD PRIMARY KEY (`partyID`),
+  ADD KEY `municipality` (`municipalityID`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- Indexes for table `vote`
+--
+ALTER TABLE `vote`
+  ADD PRIMARY KEY (`voteID`),
+  ADD KEY `code` (`codeID`),
+  ADD KEY `member` (`memberID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `municipality`
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `memberID` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `municipality`
 --
 ALTER TABLE `municipality`
-  MODIFY `municipalityID` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `municipalityID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT voor een tabel `party`
+-- AUTO_INCREMENT for table `party`
 --
 ALTER TABLE `party`
-  MODIFY `partyID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `partyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `member`
+--
+ALTER TABLE `member`
+  ADD CONSTRAINT `party` FOREIGN KEY (`partyID`) REFERENCES `party` (`partyID`);
+
+--
+-- Constraints for table `party`
+--
+ALTER TABLE `party`
+  ADD CONSTRAINT `municipality` FOREIGN KEY (`municipalityID`) REFERENCES `municipality` (`municipalityID`);
+
+--
+-- Constraints for table `vote`
+--
+ALTER TABLE `vote`
+  ADD CONSTRAINT `code` FOREIGN KEY (`codeID`) REFERENCES `code` (`codeID`),
+  ADD CONSTRAINT `member` FOREIGN KEY (`memberID`) REFERENCES `member` (`memberID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
