43323cd34a0c77fdfea421f373cd5e4d3cb87a3c
Jelle <63313141+Jellewolf@users.noreply.github.com>
2022-02-18 09:04:02 +0100

    chore: Added new pages and linked stylesheet.

---
+++ overviewparties.php
@@ -0,0 +1,18 @@
+<?php 
+    // Connection file
+    require_once "includes/config.php";   
+    // Header file  
+    require 'includes/header.inc.php';
+?>
+
+<body>
+    <div class=flex-container>
+        
+ <p>Candidate<p>
+    </div>
+</body>
+
+<?php 
+    // Footer file
+    require 'includes/footer.inc.php';
+?>
