70c94f4c2495bd6efa6ba5278f3b7eb626380f80
Jelle <63313141+Jellewolf@users.noreply.github.com>
2022-03-08 21:08:47 +0100

    fix: tried to add usedCodeExpired update

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,15 +1,17 @@
 <?php
     // Connection file
-    require_once "config.php";   
+    require_once "config.php";
+    // Session File
+    require 'session.inc.php';
         //This query checks if the code exists in the database or not
         $verifCode = $_POST['verifCode'];
         $codeUsed = "0";
+
         $sql = "SELECT * FROM code WHERE uniqueCode = ? AND codeUsed = ?;";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt,$sql)) {
             echo "SQL statement failed";
-        } 
-
+        }
             else {
                 mysqli_stmt_bind_param($stmt, "ss", $verifCode, $codeUsed);
                 mysqli_stmt_execute($stmt);
@@ -19,10 +21,29 @@
                 }
             }
             //If the code does not exist the user gets send back to the validation page
-            if (mysqli_num_rows($result) == 0) { 
+            if (mysqli_num_rows($result) == 0) {
                 header('Location: /verification.php?error=codeincorrect');
-            }
-            
 
+                /*
+                $codeUsedColumn = $row['usedAfterExpired'];
+                $updateUsedCode = 1;
+                $sql1 = "UPDATE code SET usedAfterExpired = ? + ? WHERE uniqueCode = ?;";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt,$sql1)) {
+                    echo "SQL statement failed";
+                } 
+                else {
+                        mysqli_stmt_bind_param($stmt, "iis",  $codeUsedColumn, $updateUsedCode, $verifCode);
+                        mysqli_stmt_execute($stmt);
+                        $result = mysqli_stmt_get_result($stmt);
+                        while ($row = mysqli_fetch_assoc($result)) {
+                        }
+
+                }
+                */
+            
+            }
+            $_SESSION['code'] = $verifCode;
+            $_SESSION['codeID'] = "test";
  
 ?>  
