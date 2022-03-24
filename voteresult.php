6deae07be063eb7c27a227b832c8009903c83d9d
basiking <33873299+basiking@users.noreply.github.com>
2022-03-24 10:09:04 +0100

    feat: added result page

---
+++ voteresult.php
@@ -0,0 +1,20 @@
+<?php 
+    // Connection file
+    require_once "includes/config.php";   
+    // Header file  
+    require 'includes/header.inc.php';
+?>
+<body>
+    <div class="container-1">
+        <div class="header-logo">
+            <!--This makes up the logo, its done this way so we can easily change the words dynamically-->
+            <span class="gray-text">Stem</span><span class="blue-text">Applicatie</span>
+        </div>
+        <div class="header-back">
+            <!-- This button brings the user to the party overview page-->
+        </div>
+    </div>
+<?php 
+    // Footer file
+    require 'includes/footer.inc.php';
+?>
